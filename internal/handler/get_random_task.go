6a9b709cc64f6f993fc022ffda705916ec664349
osami <v.pylayev@mail.ru>
2026-03-16 06:09:07 +0500

    update get random task handler

---
+++ internal/handler/get_random_task.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v5"
+)
+
+func (h *Handler) GetRandomTask(c *echo.Context) error {
+	levelStr := c.Param("level")
+	taskType := c.Param("type")
+
+	level, err := strconv.Atoi(levelStr)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "level must be number")
+	}
+
+	task, err := h.service.GetRandomTask(c.Request().Context(), taskType, uint(level))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, task)
+}
+
+
