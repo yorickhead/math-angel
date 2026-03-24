80d724872b19d6081df739fa784ef83d3f89619a
osami <v.pylayev@mail.ru>
2026-03-24 07:45:55 +0500

    add errors

---
+++ internal/errors/errors.go
@@ -0,0 +1,11 @@
+package errors
+
+import "errors"
+
+var (
+	ErrEmptyTask    = errors.New("empty task")
+	ErrAlreadyExist = errors.New("task already exist")
+	ErrUnknown      = errors.New("unknown error")
+	ErrNotFound     = errors.New("not found")
+	ErrBadUID = errors.New("bad uid")
+)
